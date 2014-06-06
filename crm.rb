0c75e8757db60dee169376113fb1606843cece26
Zack Fisch <zack@zfisch.com>
2014-06-06 10:55:00 -0400

    Created call option method

--- crm.rb
+++ crm.rb
@@ -14,4 +14,13 @@ def main_menu
 	# call_option(selection)
 end
 
+def call_option(selection)
+	add_new_contact if selection == 1
+	modify_contact if selection == 2
+	delete_contact if selection == 3
+	display_contacts if selection == 4
+	display attribute if selection == 5
+	exiit_crm if selection == 6
+end
+
 main_menu
